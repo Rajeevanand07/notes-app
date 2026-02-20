78592df89d91902eb3f5c77c73fed5dcb015a34b
Rajeevanand07 <anandrajeev1234@gmail.com>
2026-02-20 11:26:15 +0530

    changes made in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware   
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://notes-app-x7wq.vercel.app/'],
     credentials: true
 }));
 app.use(cookieParser())
